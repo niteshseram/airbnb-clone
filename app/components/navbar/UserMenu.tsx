f70479542f3c83ceb2f9d8a886968e952bb5bee8
Nitesh Seram <niteshseram@gmail.com>
2023-04-06 20:21:01 +0530

    Create Auth Base UI and Register Modal

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -6,7 +6,10 @@ import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar'
 import MenuItem from './MenuItem'
 
+import useRegisterModal from '@/app/hooks/useRegisterModal'
+
 const UserMenu = () => {
+  const registerModal = useRegisterModal();
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
@@ -78,7 +81,7 @@ const UserMenu = () => {
                 label="Login"
               />
               <MenuItem 
-                onClick={() => {}}
+                onClick={registerModal.onOpen}
                 label="Sign Up"
               />
             </>
